9a724e70b6bf52ba7f1f5398382ae4739fe11250
Adam Balsam <balsam.adam@gmail.com>
2017-03-28 15:45:28 -0400

    Adding other places I found refs to blt.sh

--- scripts/blt/convert-to-composer.sh
+++ scripts/blt/convert-to-composer.sh
@@ -49,8 +49,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
   # Install (new) alias
   echo "Installing blt alias"
-  yes | ./vendor/acquia/blt/blt.sh install-alias
-  ./vendor/acquia/blt/blt.sh init
+  yes | ./vendor/phing/phing/bin/phing install-alias
+  ./vendor/phing/phing/bin/phing init
   composer update
 
   echo "Update complete. Please do the following:"
